71d34d921570e1f5d32d6ca0122a0cd21c56c1ab
m00nberri <zaberkat@gmail.com>
2022-07-04 22:22:42 +0900

    WIP/Function redo push

--- script.js
+++ script.js
@@ -5,57 +5,123 @@ let buttons = document.getElementsByTagName('button');
 })
 
 function buttonHandler(e) {
-    if (memory === null) {
-        newCalc(e);
-    }
-    else if (memory !== null) {
-        continueCalc(e);
-    }
+    newCalc(e);
 }
 
 let workingData = {
     left: '',
     right: '',
     operator: '',
+    operatorDisplay: '',
 }
 let workingDisplay = document.getElementById('workingLine');
 function updateDisplay() {
-    workingDisplay.textContent = `${workingData.left} ${workingData.operator} ${workingData.right}`;
+    workingDisplay.textContent = `${workingData.left} ${workingData.operatorDisplay} ${workingData.right}`;
 }
 
-let memory = null;
+let resultDisplay = document.getElementById("resultLine");
+
+function updateResult(value) {
+    resultDisplay.textContent = value;
+}
+
+let memory = '';
 
 function newCalc(e) {
     if (e.target.classList.contains('number') && workingData.operator === '') {
         workingData.left += e.target.innerText;
         updateDisplay();
-        console.log(((workingData.left).split('.')).length)
+    }
+    else if (e.target.classList.contains('number') && workingData.operator !== '') {
+        workingData.right += e.target.innerText;
+        updateDisplay();
     }
 
     else if (e.target.classList.contains('operator') && workingData.left === '') {
         null;
     }
     else if (e.target.classList.contains('operator') && workingData.right === '') {
-        workingData.operator = e.target.innerText;
+        workingData.operatorDisplay = e.target.innerText;
+        workingData.operator = e.target.id;
         updateDisplay();
     }
     else if (e.target.classList.contains('operator') && workingData.right !== '') {
-        calculate(workingData.left,workingData.operator,workingData.right);
-        workingData.operator = e.target.innerText;
+        memory = calculate(workingData.left,workingData.operator,workingData.right);
+        updateResult(memory);
+        workingData.operatorDisplay = e.target.innerText;
+        workingData.operator = e.target.id;
         updateDisplay();
     }
 
-    else if (e.target.classList.contains('equals') && workingData.left !== '' && workingData.operator === '') {
-        calculate(workingData.left,workingData.operator,workingData.right);
+    else if (e.target.classList.contains('specialOperator') && workingData.right === '') {
+        workingData.operatorDisplay = e.target.innerText;
+        workingData.operator = e.target.id;
+        memory = calculate(workingData.left,workingData.operator,workingData.right);
+        updateResult(memory);
+    }
+    else if (e.target.classList.contains('specialOperator') && workingData.right !== '') {
+        memory = calculate(workingData.left,workingData.operator,workingData.right);
+        workingData.operator = e.target.id;
+        memory = calculate(memory,workingData.operator,workingData.right);
+        updateResult(memory);
+        
+    }
+
+    else if (e.target.classList.contains('equals') && workingData.left !== '' && workingData.right === '') {
+        memory = workingData.left;
+        updateWorkingData(memory);
+        updateResult(memory);
     }
     else if (e.target.classList.contains('equals') && workingData.left !== '' && workingData.right !== '') {
-        calculate(workingData.left,workingData.operator,workingData.right);
+        memory = calculate(workingData.left,workingData.operator,workingData.right);
+        updateResult(memory);
     }
 }
 
 function calculate(left,operator,right) {
     errorCheck();
-    if (workingData.right === '')
+    if (operator === ('positiveNegative' || 'squareRoot' || 'percent')) {
+        return specialOperation(left, operator);
+    }
+    else if ((left && right && operator) !== '') {
+        return operation(left, operator, right);
+    }
+    else {
+        alert('no calc');
+    }
+    workingDisplay.textContent = '';
+}
+
+function operation(left, operator, right) {
+    switch(operator) {
+        case 'divide':
+        workingData.operator = '';
+        return parseFloat(left/right);
+        case 'multiply':
+        workingData.operator = '';
+        return parseFloat(left*right);
+        case 'minus':
+        workingData.operator = '';
+        return parseFloat(left-right);
+        case 'plus':
+        workingData.operator = '';
+        return parseFloat(left+right);
+    }
+}
+
+function specialOperation(value, operator) {
+    alert('why');
+    switch(operator) {
+        case 'positiveNegative':
+            workingData.operator = '';
+            return parseFloat((value*(-1)));
+        case 'squareRoot':
+            workingData.operator = '';
+            return parseFloat(Math.sqrt(value));
+        case 'percent':
+            workingData.operator = '';
+            return parseFloat(value/100);
+    }
 }
 
 function errorCheck() {
@@ -65,4 +131,11 @@ function errorCheck() {
     else if (((workingData.left).split('.'))[1] === '' || ((workingData.right).split('.'))[1] === '') {
         alert('you broke it :(');
     }
+}
+
+function updateWorkingData(value) {
+    workingData.operatorDisplay = '';
+    workingData.operator = '';
+    workingData.right = '';
+    workingDisplay.textContent = `${workingData.left} ${workingData.operatorDisplay} ${workingData.right}`;
 }
