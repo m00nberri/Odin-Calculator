9ec1b482151453b13c03eb59992e6ae836d55635
m00nberri <zaberkat@gmail.com>
2022-07-05 11:50:58 +0900

    Update buttonHandler and remove previous functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let memory = '';
 let buttons = document.getElementsByTagName('button');
 
 (Array.from(buttons)).forEach(button => {
@@ -5,7 +6,27 @@ let buttons = document.getElementsByTagName('button');
 })
 
 function buttonHandler(e) {
-    newCalc(e);
+    if (e.target.classList.contains('number')) {
+        numberPress(e);
+    }
+    else if (e.target.classList.contains('specialOperator')) {
+        specialOperatorPress(e);
+    }
+    else if (e.target.classList.contains('operator')) {
+        operatorPress(e);
+    }
+    else if (e.target.classList.contains('equals')) {
+        equalsPress(e);
+    }
+    else if (e.target.id === 'del') {
+        delPress(e);
+    }
+    else if (e.target.id === 'clear') {
+        clearPress(e);
+    }
+    else {
+        alert('idk what you button you pressd')
+    }
 }
 
 let workingData = {
@@ -14,114 +35,16 @@ let workingData = {
     operator: '',
     operatorDisplay: '',
 }
+
 let workingDisplay = document.getElementById('workingLine');
-function updateDisplay() {
-    workingDisplay.textContent = `${workingData.left} ${workingData.operatorDisplay} ${workingData.right}`;
+function updateDisplay(value) {
+    workingDisplay.textContent = value;
 }
 
 let resultDisplay = document.getElementById("resultLine");
 
 function updateResult(value) {
     resultDisplay.textContent = value;
-}
-
-let memory = '';
-
-function newCalc(e) {
-    if (e.target.classList.contains('number') && workingData.operator === '') {
-        workingData.left += e.target.innerText;
-        updateDisplay();
-    }
-    else if (e.target.classList.contains('number') && workingData.operator !== '') {
-        workingData.right += e.target.innerText;
-        updateDisplay();
-    }
-
-    else if (e.target.classList.contains('operator') && workingData.left === '') {
-        null;
-    }
-    else if (e.target.classList.contains('operator') && workingData.right === '') {
-        workingData.operatorDisplay = e.target.innerText;
-        workingData.operator = e.target.id;
-        updateDisplay();
-    }
-    else if (e.target.classList.contains('operator') && workingData.right !== '') {
-        memory = calculate(workingData.left,workingData.operator,workingData.right);
-        updateResult(memory);
-        workingData.operatorDisplay = e.target.innerText;
-        workingData.operator = e.target.id;
-        updateDisplay();
-    }
-
-    else if (e.target.classList.contains('specialOperator') && workingData.right === '') {
-        workingData.operatorDisplay = e.target.innerText;
-        workingData.operator = e.target.id;
-        memory = calculate(workingData.left,workingData.operator,workingData.right);
-        updateResult(memory);
-    }
-    else if (e.target.classList.contains('specialOperator') && workingData.right !== '') {
-        memory = calculate(workingData.left,workingData.operator,workingData.right);
-        workingData.operator = e.target.id;
-        memory = calculate(memory,workingData.operator,workingData.right);
-        updateResult(memory);
-        
-    }
-
-    else if (e.target.classList.contains('equals') && workingData.left !== '' && workingData.right === '') {
-        memory = workingData.left;
-        updateWorkingData(memory);
-        updateResult(memory);
-    }
-    else if (e.target.classList.contains('equals') && workingData.left !== '' && workingData.right !== '') {
-        memory = calculate(workingData.left,workingData.operator,workingData.right);
-        updateResult(memory);
-    }
-}
-
-function calculate(left,operator,right) {
-    errorCheck();
-    if (operator === ('positiveNegative' || 'squareRoot' || 'percent')) {
-        return specialOperation(left, operator);
-    }
-    else if ((left && right && operator) !== '') {
-        return operation(left, operator, right);
-    }
-    else {
-        alert('no calc');
-    }
-    workingDisplay.textContent = '';
-}
-
-function operation(left, operator, right) {
-    switch(operator) {
-        case 'divide':
-        workingData.operator = '';
-        return parseFloat(left/right);
-        case 'multiply':
-        workingData.operator = '';
-        return parseFloat(left*right);
-        case 'minus':
-        workingData.operator = '';
-        return parseFloat(left-right);
-        case 'plus':
-        workingData.operator = '';
-        return parseFloat(left+right);
-    }
-}
-
-function specialOperation(value, operator) {
-    alert('why');
-    switch(operator) {
-        case 'positiveNegative':
-            workingData.operator = '';
-            return parseFloat((value*(-1)));
-        case 'squareRoot':
-            workingData.operator = '';
-            return parseFloat(Math.sqrt(value));
-        case 'percent':
-            workingData.operator = '';
-            return parseFloat(value/100);
-    }
 }
 
 function errorCheck() {
@@ -131,11 +54,4 @@ function errorCheck() {
     else if (((workingData.left).split('.'))[1] === '' || ((workingData.right).split('.'))[1] === '') {
         alert('you broke it :(');
     }
-}
-
-function updateWorkingData(value) {
-    workingData.operatorDisplay = '';
-    workingData.operator = '';
-    workingData.right = '';
-    workingDisplay.textContent = `${workingData.left} ${workingData.operatorDisplay} ${workingData.right}`;
 }
